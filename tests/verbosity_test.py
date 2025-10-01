e6eb687a0a4816a9d0ccbca749638c393eb1c894
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-10-01 13:36:43 -0700

    add Pytest Support: Add Tests for Verbosity Functions, Init Requirements
    [^]requirements.txt: add requirements.txt with pytest dependency
    [@]verbosity_test.py: replace getLogger import by logging and add pytest tests for verbosity helpers

--- tests/verbosity_test.py
+++ tests/verbosity_test.py
@@ -8,7 +8,7 @@ add_verbose_arguments, calc_verbosity, set_logging_level_by_verbosity
 from argparse import ArgumentParser
 import sys
 import os
-from logging import getLogger
+import logging
 
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
@@ -19,9 +19,223 @@ from kamilog.kamilog import (
     set_logging_level_by_verbosity,
 )
 
-LOGGER_NAME = "VERBOSITY_TEST"
+# pytest candidates  ###########################################################
+
+
+class TestAddVerboseArguments:
+    """
+    test ``add_verbose_arguments``
+    """
+
+    def test1(_):
+        ARGS = []
+        parser = ArgumentParser()
+
+        add_verbose_arguments(parser)
+        args = parser.parse_args(ARGS)
+
+        print(args)
+
+        assert hasattr(args, "verbose")
+        assert hasattr(args, "quiet")
+
+        assert args.verbose == 0
+        assert args.quiet == 0
+
+    def test2(_):
+        ARGS = ["-vv"]
+        parser = ArgumentParser()
+
+        add_verbose_arguments(parser)
+        args = parser.parse_args(ARGS)
+
+        print(args)
+
+        assert hasattr(args, "verbose")
+        assert hasattr(args, "quiet")
+
+        assert args.verbose == 2
+        assert args.quiet == 0
+
+    def test3(_):
+        ARGS = ["--quiet"]
+        parser = ArgumentParser()
+
+        add_verbose_arguments(parser)
+        args = parser.parse_args(ARGS)
+
+        print(args)
+
+        assert hasattr(args, "verbose")
+        assert hasattr(args, "quiet")
+
+        assert args.verbose == 0
+        assert args.quiet == 1
+
+    def test4(_):
+        ARGS = ["-vv", "--verbose", "-qq"]
+        parser = ArgumentParser()
+
+        add_verbose_arguments(parser)
+        args = parser.parse_args(ARGS)
+
+        print(args)
+
+        assert hasattr(args, "verbose")
+        assert hasattr(args, "quiet")
+
+        assert args.verbose == 3
+        assert args.quiet == 2
+
+
+class TestCalcVerbosity:
+    """
+    test ``calc_verbosity``
+    """
+
+    def test1(_):
+        ARGS = []
+        parser = ArgumentParser()
+
+        add_verbose_arguments(parser)
+        args = parser.parse_args(ARGS)
+
+        verbosity = calc_verbosity(args)
+
+        print(verbosity)
+        assert verbosity == 0
+
+    def test2(_):
+        ARGS = ["-vv"]
+        parser = ArgumentParser()
+
+        add_verbose_arguments(parser)
+        args = parser.parse_args(ARGS)
+
+        verbosity = calc_verbosity(args)
+
+        print(verbosity)
+        assert verbosity == 2
+
+    def test3(_):
+        ARGS = ["--quiet"]
+        parser = ArgumentParser()
+
+        add_verbose_arguments(parser)
+        args = parser.parse_args(ARGS)
+
+        verbosity = calc_verbosity(args)
+
+        print(verbosity)
+        assert verbosity == -1
+
+    def test4(_):
+        ARGS = ["-vv", "--verbose", "-qq"]
+        parser = ArgumentParser()
+
+        add_verbose_arguments(parser)
+        args = parser.parse_args(ARGS)
+
+        verbosity = calc_verbosity(args)
+
+        print(verbosity)
+        assert verbosity == 1
+
+
+class TestSLLBV:
+    """
+    test ``set_logging_level_by_verbosity``
+    """
+
+    def test_no(_):
+        ARGS = []
+        LOGGER_NAME = "TestSLLBV_test1"
+        parser = ArgumentParser()
+
+        add_verbose_arguments(parser)
+        args = parser.parse_args(ARGS)
+
+        set_logging_level_by_verbosity(args, LOGGER_NAME)
+        logger = logging.getLogger(LOGGER_NAME)
+        logging_level = logger.level
+
+        print(logging_level)
+        assert logging_level == logging.WARNING
+
+    def test_v1(_):
+        ARGS = ["-v"]
+        parser = ArgumentParser()
+
+        add_verbose_arguments(parser)
+        args = parser.parse_args(ARGS)
+
+        set_logging_level_by_verbosity(args, LOGGER_NAME)
+        logger = logging.getLogger(LOGGER_NAME)
+        logging_level = logger.level
+
+        print(logging_level)
+        assert logging_level == logging.INFO
+
+    def test_v2(_):
+        ARGS = ["-vv"]
+        parser = ArgumentParser()
+
+        add_verbose_arguments(parser)
+        args = parser.parse_args(ARGS)
+
+        set_logging_level_by_verbosity(args, LOGGER_NAME)
+        logger = logging.getLogger(LOGGER_NAME)
+        logging_level = logger.level
+
+        print(logging_level)
+        assert logging_level == logging.DEBUG
+
+    def test_v3(_):
+        ARGS = ["-vvv"]
+        parser = ArgumentParser()
+
+        add_verbose_arguments(parser)
+        args = parser.parse_args(ARGS)
+
+        set_logging_level_by_verbosity(args, LOGGER_NAME)
+        logger = logging.getLogger(LOGGER_NAME)
+        logging_level = logger.level
+
+        print(logging_level)
+        assert logging_level == logging.DEBUG
+
+    def test_q1(_):
+        ARGS = ["-q"]
+        parser = ArgumentParser()
+
+        add_verbose_arguments(parser)
+        args = parser.parse_args(ARGS)
+
+        set_logging_level_by_verbosity(args, LOGGER_NAME)
+        logger = logging.getLogger(LOGGER_NAME)
+        logging_level = logger.level
+
+        print(logging_level)
+        assert logging_level == logging.CRITICAL + 1
+
+    def test_q2(_):
+        ARGS = ["-qq"]
+        parser = ArgumentParser()
+
+        add_verbose_arguments(parser)
+        args = parser.parse_args(ARGS)
+
+        set_logging_level_by_verbosity(args, LOGGER_NAME)
+        logger = logging.getLogger(LOGGER_NAME)
+        logging_level = logger.level
+
+        print(logging_level)
+        assert logging_level == logging.CRITICAL + 1
+
 
 # sys.argv parser  #############################################################
+
+LOGGER_NAME = "VERBOSITY_TEST"
 
 sys_argv_parser = ArgumentParser()
 add_verbose_arguments(sys_argv_parser)
@@ -37,5 +251,5 @@ if __name__ == "__main__":
     # test set_logging_level_by_verbosity
     set_logging_level_by_verbosity(args, LOGGER_NAME)
 
-    logger = getLogger(LOGGER_NAME)
+    logger = logging.getLogger(LOGGER_NAME)
     print("logging level:\t{}".format(logger.level))
